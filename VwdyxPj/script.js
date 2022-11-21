698c38a6f4bad487e2f5826dca6dedaf7fc9ad49
zakzakst <e1433104@gmail.com>
2022-11-22 00:05:00 +0900

    VwdyxPj

--- VwdyxPj/script.js
+++ VwdyxPj/script.js
@@ -7,22 +7,16 @@
   tl.to(container, {
     x: () => -(container.scrollWidth - document.documentElement.clientWidth) + 'px',
     ease: 'none',
-  })
+    scrollTrigger: {
+      trigger: container,
+      scrub: 1,
+      start: 'top top',
+      end: '+=1200',
+      pin: true,
+      markers: {
+        startColor: 'white',
+        endColor: 'fuchsia',
+      },
+    },
+  });
 })();
-
-// gsap.registerPlugin(ScrollTrigger);
-// let container = document.querySelector(".wrapper");
-// let tl = gsap.timeline();
-// tl.to(container, {
-//   x: () =>
-//     -(container.scrollWidth - document.documentElement.clientWidth) + "px",
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: container,
-//     scrub: 1,
-//     start: "top top",
-//     end: "+=1200",
-//     pin: true,
-//     markers: { startColor: "white", endColor: "fuchsia" },
-//   },
-// });
--- memo.md
+++ memo.md
@@ -5,7 +5,7 @@ https://codepen.io/GreenSock/pens/public?grid_type=list&sort_col=created_at
 https://greensock.com/drawsvg/
 
 ## TODO
-https://codepen.io/GreenSock/pen/VwdyxPj
 
 ## DONE
 https://codepen.io/GreenSock/pen/xxzpOwa
+https://codepen.io/GreenSock/pen/VwdyxPj （pinの仕組みがいまいち分からなかった）
