98a209fb79e04ac4adba065bacd9358209b81dfa
zakzakst <e1433104@gmail.com>
2022-11-21 23:55:03 +0900

    VwdyxPj

---
+++ VwdyxPj/script.js
@@ -0,0 +1,28 @@
+(() => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const container = document.querySelector('.wrapper');
+
+  const tl = gsap.timeline();
+  tl.to(container, {
+    x: () => -(container.scrollWidth - document.documentElement.clientWidth) + 'px',
+    ease: 'none',
+  })
+})();
+
+// gsap.registerPlugin(ScrollTrigger);
+// let container = document.querySelector(".wrapper");
+// let tl = gsap.timeline();
+// tl.to(container, {
+//   x: () =>
+//     -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: container,
+//     scrub: 1,
+//     start: "top top",
+//     end: "+=1200",
+//     pin: true,
+//     markers: { startColor: "white", endColor: "fuchsia" },
+//   },
+// });
