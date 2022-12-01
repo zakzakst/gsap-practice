f9ba46d83c511ff2b65b55b7f384e99ddb5436a1
zakzakst <e1433104@gmail.com>
2022-12-02 05:30:50 +0900

    dyKKQwm

--- dyKKQwm/script.js
+++ dyKKQwm/script.js
@@ -1,29 +1,37 @@
-//View more event text animation
-let hoverBoxes = gsap.utils.toArray(".link-tax");
-
-hoverBoxes.forEach(box => {
-  let moreEvent = box.querySelector(".more-event"),
-      split = new SplitText(moreEvent, { type: "words,chars" }),
-      chars = split.chars,
-      tl = gsap.timeline({
-              paused: true,
-              scrollTrigger: {
-                trigger: box,
-                start: "top center",
-                end: "bottom top",
-                toggleActions: "restart pause resume reset",
-                markers: true
-              }
-            });
-  tl.from( chars, {
-    duration: 1,
-    opacity: 0,
-    scale: 0,
-    y: 80,
-    rotationX: 180,
-    transformOrigin: "0% 50% -50",
-    ease: "back",
-    stagger: 0.03
+(() => {
+  const hoverBoxes = gsap.utils.toArray('.link-tax');
+  hoverBoxes.forEach((box) => {
+    const moreEventEl = box.querySelector('.more-event');
+    const split = moreEventEl.textContent.split('');
+    const splitEls = split.map((item) => {
+      const itemEl = document.createElement('span');
+      itemEl.textContent = item;
+      return itemEl;
+    });
+    moreEventEl.innerHTML = '';
+    splitEls.forEach((el) => {
+      moreEventEl.appendChild(el);
+    });
+    const tl = gsap.timeline({
+      paused: true,
+      scrollTrigger: {
+        trigger: box,
+        start: 'top center',
+        end: 'bottom top',
+        toggleActions: 'restart pause resume reset',
+        markers: true,
+      },
+    });
+    tl.from(moreEventEl.children, {
+      duration: 1,
+      opacity: 0,
+      scale: 0,
+      y: 80,
+      rotateX: 180,
+      transformOrigin: '0% 50% -50',
+      ease: 'back',
+      stagger: 0.03,
+    });
+    box.addEventListener('mouseenter', () => tl.restart());
   });
-  box.addEventListener("mouseenter", () => tl.restart());
-});
+})();
