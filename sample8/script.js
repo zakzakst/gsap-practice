ef0dbc94805caa97c00cd00619c92dbfee3ef6d5
zakzakst <e1433104@gmail.com>
2023-01-28 17:44:54 +0900

    sample8

---
+++ sample8/script.js
@@ -0,0 +1,41 @@
+(() => {
+  const mainEl = document.querySelector('.main');
+  const sectionTargetEl = document.querySelector('.section.--target');
+
+  const bgChangeBlack = () => {
+    gsap.to(mainEl, {
+      backgroundColor: '#4a4a4a',
+    });
+    gsap.to(sectionTargetEl, {
+      color: '#fff',
+    });
+  };
+
+  const bgChangeWhite = () => {
+    gsap.to(mainEl, {
+      backgroundColor: '#fff',
+    });
+    gsap.to(sectionTargetEl, {
+      color: '#4a4a4a',
+    });
+  };
+
+  ScrollTrigger.create({
+    trigger: sectionTargetEl,
+    start: 'top 50%',
+    end: 'bottom 50%',
+    onEnter: () => {
+      bgChangeBlack();
+    },
+    onEnterBack: () => {
+      bgChangeBlack();
+    },
+    onLeave: () => {
+      bgChangeWhite();
+    },
+    onLeaveBack: () => {
+      bgChangeWhite();
+    },
+    // markers: true,
+  });
+})();
