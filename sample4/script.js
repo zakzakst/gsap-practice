803cc020fcd855a5204e28f348906f5edaa22d9f
zakzakst <e1433104@gmail.com>
2023-01-28 11:51:10 +0900

    sample4

--- sample4/script.js
+++ sample4/script.js
@@ -1,3 +1,21 @@
 (() => {
-  console.log('script');
+  const sectionEls = document.querySelectorAll('.section');
+
+  [...sectionEls].forEach((sectionEl) => {
+    const buttonEl = sectionEl.querySelector('button');
+
+    gsap.set(buttonEl, {
+      opacity: 0,
+      scale: 1.2,
+    });
+
+    gsap.to(buttonEl, {
+      scrollTrigger: {
+        trigger: buttonEl,
+        start: 'top center',
+      },
+      scale: 1,
+      opacity: 1,
+    });
+  });
 })();
